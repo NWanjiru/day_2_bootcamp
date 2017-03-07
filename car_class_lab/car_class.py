3a4b6f6a4e18234d4cdb27b196dba79e20c9c898
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-07 22:55:31 +0300

    Made a few changes on class.py

--- car_class_lab/car_class.py
+++ car_class_lab/car_class.py
@@ -1,18 +1,28 @@
 class Car(object):
-  """Takes in the type, model and name of a vehicle and uses those arguments to 
-  instansiate different vehicles"""
-  
-    num_of_doors = 4
-    
-    def __init__(self, model = 'GM', name = 'General', car_type = 'saloon', speed = 0):
-        self.car_type = car_type
-        self.model = model
-        self.name = Car(name)
-        self.speed = speed
-        
-    def Car(self,name,model,car_type,speed):
-      if self.name == 'porshe' or self.name == 'Koenigsegg':
-        self.num_doors = 2
-        return(self.num_of_doors)
-      else:
-        return(self.num_of_doors)
+	"""Takes in the type, model and name of a vehicle and uses those arguments to instansiate different vehicles"""
+	num_of_doors = 4
+	num_of_wheels = 4
+	car_list = []
+	def __init__(self, name='General', model ='GM', car_type = None, speed = 0):
+		Car.car_list.append(self)
+		self.name = name
+		self.model = model
+		self.car_type = car_type
+		self.speed = speed
+
+	def car_properties(self,car_type):
+		
+		if car_type == 'Porshe' or car_type == 'Koenigsegg':
+			self.num_of_doors = 2
+			return self.num_of_doors
+
+
+		if car_type == 'trailer':
+			self.num_of_wheels = 8
+			return self.num_of_wheels
+		else:
+			return self.num_of_wheels
+			
+
+
+	
--- car_class_lab/test_car_class.py
+++ car_class_lab/test_car_class.py
@@ -1,4 +1,5 @@
 import unittest
+from car_class import Car
 
 class CarClassTest(unittest.TestCase):
     """docstring for CarClassTest"""
@@ -72,3 +73,6 @@ class CarClassTest(unittest.TestCase):
         self.assertListEqual([True, True, man.speed],
                              [moving_man_instance, moving_man_type, moving_man.speed],
                              msg='The car drive function should return the instance of the Car class')
+
+if __name__ == '__main__':
+    unittest()
